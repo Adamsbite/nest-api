7260e3021bfb86e5e1d91580cbf4195327d4e809
bright adams <btadamz@gmail.com>
2024-07-14 23:58:57 +0100

    lll

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { UserService } from './user/user.service';
+import { ProductService } from './product/product.service';
+import { ProductController } from './product/product.controller';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, ProductModule],
 
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, ProductController],
+  providers: [AppService, UserService, ProductService],
 })
 export class AppModule {}
